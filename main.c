e9f542b0ce00ab6a123a94e92dfdb4496c66d3f1
danielrrv <drodrigo678@gmail.com>
2020-12-27 15:43:23 -0500

    [Reader] Reading csv successfully.

--- main.c
+++ main.c
@@ -72,60 +72,73 @@ void printGraph(Graph *graph)
 	}
 };
 
-//!TODO:Reactivate after get csv dimmensions
-double **reader(char *);
+
+double **reader(char *, size_t *, size_t *);
 int main(int argc, char *argv[])
 {
-	struct Graph *graph = createGraph(8);
+
+	FILE *file;
+	char buffer[1024];
+	file = fopen("data.txt", "r");
+	size_t column;
+	for (int i = 0; fgets(buffer, 1024, file); i++)
+		;
+	const char *tok;
+	char delim[] = {","};
+	int j = 0;
+	tok = strtok(buffer, delim);
+	while (tok != NULL)
+	{
+		//printf("\nToken : %s\n", tok);
+		tok = strtok(NULL, delim);
+		column = ++j;
+	}
+
+	size_t row = sizeof(file) - 1;
+	double **data = reader("data.txt", &row, &column);
+	Graph *graph = createGraph(column);
 	addEdge(graph, 2, 4, 1, 0.323);
 	addEdge(graph, 1, 3, 1, 0.4);
 	addEdge(graph, 2, 5, 32.3, 0.05);
 	addEdge(graph, 7, 2, 32.3, 0.05);
 	addEdge(graph, 1, 3, 32.3, 0.05);
 	addEdge(graph, 8, 4, 32.3, 0.05);
-	printGraph(graph);
+	//printGraph(graph);
 
-	FILE *file;
-	char buffer[1024];
-	const char* header;
-	file = fopen("data.txt", "r");
-	size_t row;
-	for (int i = 0; fgets(buffer, 1024, file); i++)
-	{
-		header = buffer; 
-		printf("line %d:\t%s", i, buffer);
-	}
-	const char *tok;
-	const char delim[2] = ",";
-	int j = 0;
-	while(tok = strtok(header, delim))
-	{
-		if(tok != NULL)break;
-		
-		printf("\nToken : %s\n", tok);
-		tok = strtok(NULL, header);
-		row = ++j;
-	}
-
-	size_t len = sizeof(file);
-	printf("%ld\n", len);
-	printf("%ld", row);
 	return 0;
 }
-//!TODO:Reactivate after get csv dimmensions
-double **reader(char *filename)
+
+double **reader(char *filename, size_t *row, size_t *column)
 {
 
 	FILE *file;
 	double **data;
-	data = (double *)malloc(3 * sizeof(double *));
-	for (int i=0;i < )
+	data = (double **)malloc((*row) * sizeof(double *));
+	for (int i = 0; i < (*row); ++i)
+	{
+		data[i] = (double *)malloc((*column) * sizeof(double));
+	}
 	file = fopen(filename, "r");
-
 	char buffer[1024];
 	int i = 0;
-	for (; fgets(buffer, 1024, file) && (i < row);)
-		;
+	for (; fgets(buffer, 1024, file) && (i < (*row));)
+	{
+		char *tmp = strdup(buffer);
+		if (i > 0)
+		{
+			int j = 0;
+			const char *tok;
+			char delim[] = {","};
+			for (tok = strtok(buffer, delim); tok && *tok; j++, tok = strtok(NULL, delim))
+			{
+				data[i][j] = atof(tok);
+				printf("%f\t", data[i][j]);
+			}
+			printf("\n");
+		}
 
+		free(tmp);
+		i++;
+	}
 	return data;
 }
